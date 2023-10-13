e71e72f988c3d05dbb16461822b4c87339570766
BuildTools <unconfigured@null.spigotmc.org>
2023-10-13 18:48:38 +0800

    2023/10/13

--- pages/mods/unreal_core/pages/UCM_DownLoad.vue
+++ pages/mods/unreal_core/pages/UCM_DownLoad.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
 
+import {useUnrealCoreModDownLoad} from "~/stores/mods/unreal_core/useUnrealCoreModDownLoad";
+
+const unrealCoreModDownLoad = useUnrealCoreModDownLoad();
+const items = ref([{ name: 'Foo' }, { name: 'Bar' }])
+
 </script>
 
 <template>
   <p class="text-4xl dark:text-white">DownLoad</p>
+
+
+  <div class="grid grid-cols-1 lg:grid-cols-4">
+    <label for="countries" class="block mb-2 text-xl font-medium text-gray-900 dark:text-white">Filter by</label>
+    <select v-model="unrealCoreModDownLoad.data.filter_minecraft_version" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <option value="All">All Game Versions</option>
+      <option value="1.20.2">1.20.2</option>
+      <option value="1.19.4">1.19.4</option>
+      <option value="1.18.2">1.18.2</option>
+      <option value="1.17.1">1.17.1</option>
+      <option value="1.16.5">1.16.5</option>
+    </select>
+    <select v-model="unrealCoreModDownLoad.data.filter_mod_loaders" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <option value="All">All Mod Loaders</option>
+      <option value="Forge">Forge</option>
+      <option value="Fabric">Fabric</option>
+    </select>
+  </div>
+
+
+
 
 
   <div class="relative overflow-x-auto shadow-md sm:rounded-lg py-5">
@@ -11,6 +37,10 @@
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 
       <tr>
+        <th scope="col" class="px-6 py-3">
+          <p class="text-base dark:text-white">名稱</p>
+          <p class="text-base dark:text-white">Name</p>
+        </th>
         <th scope="col" class="px-6 py-3">
           <p class="text-base dark:text-white">版本</p>
           <p class="text-base dark:text-white">Version</p>
@@ -36,81 +66,33 @@
       </thead>
       <tbody>
 
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+      <tr v-for="(item, index) in unrealCoreModDownLoad.selectResults" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          <p class="text-base dark:text-white">1.0.7.1</p>
+          <p class="text-base dark:text-white">{{item.name}}</p>
         </th>
         <td class="px-6 py-4">
-          <p class="text-base dark:text-white">1.20.1</p>
+          <p class="text-base dark:text-white">{{item.mod_version}}</p>
         </td>
         <td class="px-6 py-4">
-          <p class="text-base dark:text-white">Forge</p>
+          <p class="text-base dark:text-white">{{item.minecraft_version}}</p>
         </td>
         <td class="px-6 py-4">
-          <p class="text-base dark:text-white">2023/08/06</p>
+          <p class="text-base dark:text-white">{{item.mod_loaders}}</p>
         </td>
         <td class="px-6 py-4">
-          <a href="https://cdn.discordapp.com/attachments/1020195382787059793/1137706490698928188/UnrealCore-Forge-1.20.1-1.0.7.1.jar"><i class="text-xl dark:text-white fa-regular fa-circle-down"></i></a>
+          <p class="text-base dark:text-white">{{item.uploaded}}</p>
+        </td>
+        <td class="px-6 py-4">
+          <a :href="item.download_link"><i class="text-xl dark:text-white fa-regular fa-circle-down"></i></a>
         </td>
       </tr>
 
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          <p class="text-base dark:text-white">1.0.7</p>
-        </th>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white">1.20.1</p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white">Fabric</p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white">2023/08/05</p>
-        </td>
-        <td class="px-6 py-4">
-          <a href="https://cdn.discordapp.com/attachments/1020195382787059793/1137368763784757368/UnrealCore-Fabric-1.20.1-1.0.7.jar"><i class="text-xl dark:text-white fa-regular fa-circle-down"></i></a>
-        </td>
-      </tr>
-
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          <p class="text-base dark:text-white">1.0.7</p>
-        </th>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white">1.16.5</p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white">Forge</p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white">2023/08/04</p>
-        </td>
-        <td class="px-6 py-4">
-          <a href="https://cdn.discordapp.com/attachments/1020195382787059793/1136952523882762312/UnrealCore-Forge-1.16.5-1.0.7.jar"><i class="text-xl dark:text-white fa-regular fa-circle-down"></i></a>
-        </td>
-      </tr>
-
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          <p class="text-base dark:text-white"></p>
-        </th>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white"></p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white"></p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white"></p>
-        </td>
-        <td class="px-6 py-4">
-          <p class="text-base dark:text-white"></p>
-        </td>
-      </tr>
 
       </tbody>
     </table>
   </div>
+
+  <br/> <br/> <br/>
 
 </template>
 
---
+++ stores/mods/unreal_core/useUnrealCoreModDownLoad.js
@@ -0,0 +1,257 @@
+import { defineStore } from 'pinia'
+
+export const useUnrealCoreModDownLoad = defineStore('useUnrealCoreModDownLoad', () => {
+
+
+    const selectResults = computed(() => {
+
+        let result = data.downloadList;
+
+        if(data.filter_minecraft_version !== "All"){
+            result = result.filter(item=>item.minecraft_version.includes(data.filter_minecraft_version));
+        }
+
+        if(data.filter_mod_loaders !== "All"){
+            result = result.filter(item=>item.mod_loaders.includes(data.filter_mod_loaders));
+        }
+
+        return result;
+    })
+
+    const data = reactive({
+        filter_minecraft_version: "All",
+        filter_mod_loaders: "All",
+        downloadList: [
+            {
+                name: "UnrealCore-Fabric-1.20.2-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.20.2",
+                mod_loaders: "Fabric",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326522925948969/UnrealCore-Fabric-1.20.2-1.0.8.jar?ex=653b87ee&is=652912ee&hm=5cd41418bd324a5b47d0af41f1e4f5add3b1580f22dff3024186b58b8be43c24&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.20.2-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.20.2",
+                mod_loaders: "Forge",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326521600540772/UnrealCore-Forge-1.20.2-1.0.8.jar?ex=653b87ed&is=652912ed&hm=cc17261e2094d425b0d8a58e8443a5f2cfe736d7273ada1285d0338d9e5609d5&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Fabric-1.19.4-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.19.4",
+                mod_loaders: "Fabric",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326522623950848/UnrealCore-Fabric-1.19.4-1.0.8.jar?ex=653b87ed&is=652912ed&hm=fd273ae9f2400d2aee677f3c1518afb5dbd0238da22499e0b21e9604504d3753&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.19.4-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.19.4",
+                mod_loaders: "Forge",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326521172725760/UnrealCore-Forge-1.19.4-1.0.8.jar?ex=653b87ed&is=652912ed&hm=a48244f9094794cf05f4ec7cb9965e4b75483c8587d05fa02347a30b1aebade7&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Fabric-1.18.2-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.18.2",
+                mod_loaders: "Fabric",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326522275827752/UnrealCore-Fabric-1.18.2-1.0.8.jar?ex=653b87ed&is=652912ed&hm=aa3011bbed658efe115730f1ae2d7305aa10334fbc8de988c62d8004ac084ab8&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.18.2-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.18.2",
+                mod_loaders: "Forge",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326520824602635/UnrealCore-Forge-1.18.2-1.0.8.jar?ex=653b87ed&is=652912ed&hm=d784bb2ecf8c4cdba80aa56acd70fd76273aa7f477b5ad2fcbc294b006ad5edd&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Fabric-1.17.1-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.17.1",
+                mod_loaders: "Fabric",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326521965453322/UnrealCore-Fabric-1.17.1-1.0.8.jar?ex=653b87ed&is=652912ed&hm=d5377fe75267f8e9d5054fefc2d8447eae30f1c5f2a69b2df07f7c422b15aafb&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.17.1-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.17.1",
+                mod_loaders: "Forge",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326520287723530/UnrealCore-Forge-1.17.1-1.0.8.jar?ex=653b87ed&is=652912ed&hm=c3a546f079b1d3d721702bc4f4d3c714d43ff5869eadf27623fb0eb73effbc48&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.16.5-1.0.8",
+                mod_version: "1.0.8",
+                minecraft_version: "1.16.5",
+                mod_loaders: "Forge",
+                uploaded: "2023/10/13",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326519855718451/UnrealCore-Forge-1.16.5-1.0.8.jar?ex=653b87ed&is=652912ed&hm=cfe741277ebe1167f1ac3679ae3a7befc64dd0d4810a22ad9af73b78e5bb487a&",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.20.1-1.0.7.1",
+                mod_version: "1.0.7.1",
+                minecraft_version: "1.20.1",
+                mod_loaders: "Forge",
+                uploaded: "2023/08/06",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1137706490698928188/UnrealCore-Forge-1.20.1-1.0.7.1.jar",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Fabric-1.20.1-1.0.7",
+                mod_version: "1.0.7",
+                minecraft_version: "1.20.1",
+                mod_loaders: "Fabric",
+                uploaded: "2023/08/05",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1137368763784757368/UnrealCore-Fabric-1.20.1-1.0.7.jar",
+                download_times: ""
+            },
+            {
+                name: "UnrealCore-Forge-1.16.5-1.0.7",
+                mod_version: "1.0.7",
+                minecraft_version: "1.16.5",
+                mod_loaders: "Forge",
+                uploaded: "2023/08/04",
+                download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1136952523882762312/UnrealCore-Forge-1.16.5-1.0.7.jar",
+                download_times: ""
+            }
+        ],
+    })
+
+
+
+
+    const downloadList = ref([
+        {
+            name: "UnrealCore-Fabric-1.20.2-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.20.2",
+            mod_loaders: "Fabric",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326522925948969/UnrealCore-Fabric-1.20.2-1.0.8.jar?ex=653b87ee&is=652912ee&hm=5cd41418bd324a5b47d0af41f1e4f5add3b1580f22dff3024186b58b8be43c24&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.20.2-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.20.2",
+            mod_loaders: "Forge",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326521600540772/UnrealCore-Forge-1.20.2-1.0.8.jar?ex=653b87ed&is=652912ed&hm=cc17261e2094d425b0d8a58e8443a5f2cfe736d7273ada1285d0338d9e5609d5&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Fabric-1.19.4-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.19.4",
+            mod_loaders: "Fabric",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326522623950848/UnrealCore-Fabric-1.19.4-1.0.8.jar?ex=653b87ed&is=652912ed&hm=fd273ae9f2400d2aee677f3c1518afb5dbd0238da22499e0b21e9604504d3753&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.19.4-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.19.4",
+            mod_loaders: "Forge",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326521172725760/UnrealCore-Forge-1.19.4-1.0.8.jar?ex=653b87ed&is=652912ed&hm=a48244f9094794cf05f4ec7cb9965e4b75483c8587d05fa02347a30b1aebade7&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Fabric-1.18.2-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.18.2",
+            mod_loaders: "Fabric",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326522275827752/UnrealCore-Fabric-1.18.2-1.0.8.jar?ex=653b87ed&is=652912ed&hm=aa3011bbed658efe115730f1ae2d7305aa10334fbc8de988c62d8004ac084ab8&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.18.2-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.18.2",
+            mod_loaders: "Forge",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326520824602635/UnrealCore-Forge-1.18.2-1.0.8.jar?ex=653b87ed&is=652912ed&hm=d784bb2ecf8c4cdba80aa56acd70fd76273aa7f477b5ad2fcbc294b006ad5edd&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Fabric-1.17.1-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.17.1",
+            mod_loaders: "Fabric",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326521965453322/UnrealCore-Fabric-1.17.1-1.0.8.jar?ex=653b87ed&is=652912ed&hm=d5377fe75267f8e9d5054fefc2d8447eae30f1c5f2a69b2df07f7c422b15aafb&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.17.1-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.17.1",
+            mod_loaders: "Forge",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326520287723530/UnrealCore-Forge-1.17.1-1.0.8.jar?ex=653b87ed&is=652912ed&hm=c3a546f079b1d3d721702bc4f4d3c714d43ff5869eadf27623fb0eb73effbc48&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.16.5-1.0.8",
+            mod_version: "1.0.8",
+            minecraft_version: "1.16.5",
+            mod_loaders: "Forge",
+            uploaded: "2023/10/13",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1162326519855718451/UnrealCore-Forge-1.16.5-1.0.8.jar?ex=653b87ed&is=652912ed&hm=cfe741277ebe1167f1ac3679ae3a7befc64dd0d4810a22ad9af73b78e5bb487a&",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.20.1-1.0.7.1",
+            mod_version: "1.0.7.1",
+            minecraft_version: "1.20.1",
+            mod_loaders: "Forge",
+            uploaded: "2023/08/06",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1137706490698928188/UnrealCore-Forge-1.20.1-1.0.7.1.jar",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Fabric-1.20.1-1.0.7",
+            mod_version: "1.0.7",
+            minecraft_version: "1.20.1",
+            mod_loaders: "Fabric",
+            uploaded: "2023/08/05",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1137368763784757368/UnrealCore-Fabric-1.20.1-1.0.7.jar",
+            download_times: ""
+        },
+        {
+            name: "UnrealCore-Forge-1.16.5-1.0.7",
+            mod_version: "1.0.7",
+            minecraft_version: "1.16.5",
+            mod_loaders: "Forge",
+            uploaded: "2023/08/04",
+            download_link: "https://cdn.discordapp.com/attachments/1020195382787059793/1136952523882762312/UnrealCore-Forge-1.16.5-1.0.7.jar",
+            download_times: ""
+        }
+    ])
+
+
+
+    onMounted(() => {
+
+    })
+
+    return { downloadList, data, selectResults }
+})
