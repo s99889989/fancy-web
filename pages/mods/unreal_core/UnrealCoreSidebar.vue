9a5792ddddedefa7efd7badadf1f3a577bcb27ab
BuildTools <unconfigured@null.spigotmc.org>
2023-10-15 20:41:19 +0800

    2023/10/15

--- pages/mods/unreal_core/UnrealCoreSidebar.vue
+++ pages/mods/unreal_core/UnrealCoreSidebar.vue
@@ -33,11 +33,11 @@ const unrealCoreModPages = useUnrealCoreModPages();
           </a>
         </li>
 
-<!--        <li>-->
-<!--          <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">-->
-<!--            <span class="flex-1 ml-3 whitespace-nowrap">CurseForge</span>-->
-<!--          </a>-->
-<!--        </li>-->
+        <li>
+          <a target="_blank" href="https://www.curseforge.com/minecraft/mc-mods/geckolib/files" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <span class="flex-1 ml-3 whitespace-nowrap">GeckoLib</span>
+          </a>
+        </li>
 
       </ul>
     </div>
