152d3d987878b92b24a675d4fd01f961cd9702ea
BuildTools <unconfigured@null.spigotmc.org>
2023-10-16 21:52:13 +0800

    2023/10/16

--- pages/plugins/custom_display/pages/CDI_Video.vue
+++ pages/plugins/custom_display/pages/CDI_Video.vue
@@ -4,16 +4,16 @@
 
 <template>
   <div class="grid grid-cols-1 lg:grid-cols-2">
-    <div class="aspect-w-16 aspect-h-9">
-      <iframe src="https://www.youtube.com/embed/w7-1dFO2-OY?si=QSOtoni7-PzC4Dg1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="aspect-w-16 aspect-h-9 m-10">
+      <iframe src="https://www.youtube.com/embed/S2_6WkyRx80?si=bDnmCuB2zC_8Enq1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="aspect-w-16 aspect-h-9">
+    <div class="aspect-w-16 aspect-h-9 m-10">
       <iframe src="https://www.youtube.com/embed/GpG3GWVkTtQ?si=A1KRZ7C5j6Kx7Rma" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="aspect-w-16 aspect-h-9">
+    <div class="aspect-w-16 aspect-h-9 m-10">
       <iframe src="https://www.youtube.com/embed/XzmHSMQt5VE?si=HVCk53oz34TPF99Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="aspect-w-16 aspect-h-9">
+    <div class="aspect-w-16 aspect-h-9 m-10">
       <iframe src="https://www.youtube.com/embed/lJoIkLyCDmE?si=zkIRntnXoob9t9m4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
