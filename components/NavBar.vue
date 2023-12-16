cccb8152c8d0eee0f2bce4736a792672f80d53e2
BuildTools <unconfigured@null.spigotmc.org>
2023-12-16 22:38:55 +0800

    2023/12/16

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -244,6 +244,11 @@ onMounted(()=>{
             <NuxtLink class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" target="_blank" to="https://discord.gg/dXee2YTpbn">Discord</NuxtLink>
           </li>
 
+          <li class="flex">
+            <i class="dark:text-white fa-brands fa-qq py-1"></i>
+            <NuxtLink class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" target="_blank" to="https://qm.qq.com/q/2w8E6ZBDGQ">QQ</NuxtLink>
+          </li>
+
         </ul>
       </div>
     </div>
